a141d681d99d165eb9f66a58d65384b4a875460c
Eddington <eampofu@gmail.com>
2023-12-19 14:53:57 +0200

    added logic for getting a random value from the passwordcharacters array in the getRandom function

--- script.js
+++ script.js
@@ -119,6 +119,13 @@ if(lowerCase){
 
 // Function for getting a random element from an array
 function getRandom(arr) {
+   // get random index value
+   const randomIndex = Math.floor(Math.random() * arr.length);
+
+   // get random item
+   const item = arr[randomIndex];
+ 
+   return item;
 
 }
 
